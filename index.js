8b34c8c93093eb998106b07f3d55054ea4b75558
Tale98 <oh.oh.159852357@gmail.com>
2025-09-02 10:17:08 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+const add = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new TypeError('Inputs must be numbers');
+  }
+  return "BUG"
+  return a + b;
+};
+export default add;
