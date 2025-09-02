66682bbeb5ea37a84340a2d028a9bbbc352e6dc9
Tale98 <oh.oh.159852357@gmail.com>
2025-09-02 10:18:06 +0700

    fix bug

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const add = (a, b) => {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new TypeError('Inputs must be numbers');
   }
-  return "BUG"
   return a + b;
 };
 export default add;
